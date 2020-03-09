fdbb3a06ee3ad3c743f42b2006040abeaac93d70
borisNk <borisnk@icloud.com>
2020-03-09 22:30:06 +0100

    Added functionality and permission ask for using camera to take photo and save as profile picture. Added UIAlertController for choosing between rename or delete person.

--- Names to Faces/ViewController.swift
+++ Names to Faces/ViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     var people = [Person]()
+    var indexPersonToOperate: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +22,13 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }
+        
         present(picker, animated: true)
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return people.count
     }
@@ -32,23 +37,24 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             fatalError("Unable to dequeue PersonCell.")
         }
-
+        
         let person = people[indexPath.item]
-
+        
         cell.name.text = person.name
-
+        
         let path = getDocumentsDirectory().appendingPathComponent(person.image)
         cell.imageView.image = UIImage(contentsOfFile: path.path)
-
+        
         cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
         cell.imageView.layer.borderWidth = 2
         cell.imageView.layer.cornerRadius = 3
         cell.layer.cornerRadius = 7
-
+        
         return cell
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
         guard let image = info[.editedImage] as? UIImage else { return }
         
         let imageName = UUID().uuidString
@@ -70,21 +76,41 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         return paths[0]
     }
     
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let person = people[indexPath.item]
-        
+    fileprivate func renamePerson(action: UIAlertAction) {
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
         ac.addTextField()
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.addAction(UIAlertAction(title: "OK", style: .default) {
             [weak self, weak ac] _ in
             guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
+            self?.people[self!.indexPersonToOperate].name = newName
             
             self?.collectionView.reloadData()
         })
         
         present(ac, animated: true)
     }
+    
+    fileprivate func deletePerson(action: UIAlertAction) {
+        let ac = UIAlertController(title: "Delete person", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.addAction(UIAlertAction(title: "OK", style: .default) {
+            [weak self] _ in
+            
+            self?.people.remove(at: self!.indexPersonToOperate)
+            self?.collectionView.reloadData()
+        })
+        
+        present(ac, animated: true)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        indexPersonToOperate = indexPath.item
+        
+        let ac = UIAlertController(title: "Choose operation", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Rename", style: .default, handler: renamePerson))
+        ac.addAction(UIAlertAction(title: "Delete", style: .default, handler: deletePerson))
+        
+        present(ac, animated: true)
+    }
 }
-
